87f3884877d12337b21213585d0171ae2d43d901
Sebas Sanchez <sanchezz3s47@gmail.com>
2025-08-05 17:42:21 -0500

    change photo

--- src/assets/components/AboutMe/AboutMe.jsx
+++ src/assets/components/AboutMe/AboutMe.jsx
@@ -1,7 +1,7 @@
 import { useTranslation } from "react-i18next";
 import { ReactTyped } from "react-typed";
 import './AboutMe.css';
-import photo from '/images/principal.jpg';
+import photo from '/images/principal.jpeg';
 import cvEN from '/cvs/CV-Sebastian-Sanchez-English.pdf';
 import cvES from '/cvs/CV-Sebastian-Sanchez-Espanol.pdf';
 import cvPT from '/cvs/CV-Sebastian-Sanchez-Portugues.pdf';
